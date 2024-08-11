25c5c51c64383ed1c17048883a1f9f9c6ae0461e
Victor Zadorozhnyy <antibioticvz@gmail.com>
2024-08-11 12:35:34 +0300

    config next js

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import 'server-only'
+
+export const experimental_ppr = true
+
 const Home = () => {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col">
-        <h1 className="text-3xl font-bold sm:text-5xl">World Wide</h1>
+        <h1 className="text-3xl font-bold sm:text-5xl">World Wide Hello</h1>
       </main>
       <footer className=""></footer>
     </div>
